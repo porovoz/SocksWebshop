660fbc2d95dd8ad4ccc67916c2dae2c760ae1d4b
porovoz <falkon1@list.ru>
2023-03-24 13:06:43 +0300

    Initial commit

---
+++ src/main/java/com/bestapp/coursework03sockswebshop/Coursework03SocksWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.coursework03sockswebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework03SocksWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework03SocksWebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestapp/coursework03sockswebshop/Coursework03SocksWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestapp.coursework03sockswebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework03SocksWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
